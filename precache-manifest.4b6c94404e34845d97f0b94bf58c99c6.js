9fc5ee0f6f230945d8d010044f207e722cd3c8d9
Alina <alina90565@gmail.com>
2019-10-20 13:38:45 +0300

    Updates

--- precache-manifest.4b6c94404e34845d97f0b94bf58c99c6.js
+++ precache-manifest.4b6c94404e34845d97f0b94bf58c99c6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11b40501436fd0813f7feffc0c7138a5",
+    "revision": "370379adf4a461f7cba1945ade2130d4",
     "url": "/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "fce93f84c9e325996c92",
-    "url": "/goit-react-hw-01-components/static/css/main.c114f644.chunk.css"
+    "revision": "fda5c8fdc0401f6a45df",
+    "url": "/goit-react-hw-01-components/static/css/main.8c45f14b.chunk.css"
   },
   {
     "revision": "7490eca4c7869a71b036",
     "url": "/goit-react-hw-01-components/static/js/2.fa436125.chunk.js"
   },
   {
-    "revision": "fce93f84c9e325996c92",
-    "url": "/goit-react-hw-01-components/static/js/main.95f9147b.chunk.js"
+    "revision": "fda5c8fdc0401f6a45df",
+    "url": "/goit-react-hw-01-components/static/js/main.a0428fdb.chunk.js"
   },
   {
     "revision": "4d9ce36fabe79d32c0ed",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.56f0cf13338c8cba590c8bef41f48ac3.js"
+  "/goit-react-hw-01-components/precache-manifest.4b6c94404e34845d97f0b94bf58c99c6.js"
 );
 
 self.addEventListener('message', (event) => {
